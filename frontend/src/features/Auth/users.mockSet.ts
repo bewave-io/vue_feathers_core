25289734387593990a6bc23bd77e1c6b55d4b8ec
boumaza-samir <62759258+boumaza-samir@users.noreply.github.com>
2024-02-21 12:10:54 -0500

    Clean code

--- frontend/src/features/Auth/users.mockSet.ts
+++ frontend/src/features/Auth/users.mockSet.ts
@@ -1,17 +1,21 @@
+// Need to add fullname because it's not in the user model
+
 export default {
   data: [
     {
     _id:13,
-    firstName: 'test',
-    lastName: 'test',
+    firstName: 'Paul',
+    lastName: 'Dupont',
+    fullName: 'Paul Dupont',
     email: 'test@test.fr',
     avatarUrl: 'https://www.gravatar.com/avatar/1d2ab164559aaf8a30eebf516d2f63ad',
     password: '$2a$10$JGXGRR3ouo9GJ2leSzjcR.pWHR9LhIvZmuiEmxQwwmwFz8kUEUZE.'
   },
     {
     _id:14,
-    firstName: 'test2',
-    lastName: 'test2',
+    firstName: 'Jean',
+    lastName: 'Durand',
+    fullName: 'Jean Durand',
     email: 'test@test.fr2',
     avatarUrl: 'https://www.gravatar.com/avatar/1d2ab164559aaf8a30eebf516d2f63ad',
     password: '$2a$10$JGXGRR3ouo9GJ2leSzjcR.pWHR9LhIvZmuiEmxQwwmwFz8kUEUZE.'
